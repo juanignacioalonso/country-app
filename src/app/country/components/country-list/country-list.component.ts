0001465ec0fc07730dc35da91b620f4453eb458e
juanignacioalonso <zalbak87@gmail.com>
2025-12-03 12:33:57 -0300

    Buscar países 126

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -1,10 +1,11 @@
 import { DecimalPipe } from '@angular/common';
 import { Country } from '../../interfaces/country.interface';
 import { Component, input } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'country-list',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe, RouterLink],
   templateUrl: './country-list.component.html',
 })
 export class CountryListComponent {
