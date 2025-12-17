ec8c9ed328561a372b141b39a657d3d6a715986f
juanignacioalonso <zalbak87@gmail.com>
2025-12-17 11:26:18 -0300

    Informacion de un país

--- src/app/country/pages/coutry-page/coutry-page.component.ts
+++ src/app/country/pages/coutry-page/coutry-page.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-coutry-page',
   imports: [],
   templateUrl: './coutry-page.component.html',
 })
-export class CoutryPageComponent { }
+export class CoutryPageComponent {
+
+  countryCode = inject(ActivatedRoute).snapshot.params['code'];
+  countryService = inject(CountryService);
+
+  countryResource = rxResource({
+    request:()=> ({ code:this.countryCode}),
+    loader: ({request}) =>{
+      return this.countryService.searchCountryByAlphaCode(request.code)
+    }
+  })
+}
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -50,4 +50,24 @@ export class CountryService {
 
   }
 
+    searchCountryByAlphaCode(code: string){
+
+    const url = `${API_URL}/alpha/${code}`;
+
+
+
+    return this.http.get<RESTCountry[]>(url).pipe(
+        map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        map(countries => countries.at(0)),
+        catchError(error =>{
+          console.log('Error fetching',error);
+
+          return throwError(
+            ()=> new Error(`No se pudo obtener países con ese código: ${code}`)
+          );
+        })
+      );
+
+  }
+
 }
