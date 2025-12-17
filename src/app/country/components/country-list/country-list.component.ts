dcdf202709ac2c4fe4ee229624adfee42f17ddf7
juanignacioalonso <zalbak87@gmail.com>
2025-12-17 10:51:05 -0300

    Mostrar diferentes estados al usuario

--- src/app/country/components/country-list/country-list.component.ts
+++ src/app/country/components/country-list/country-list.component.ts
@@ -9,7 +9,10 @@ import { RouterLink } from "@angular/router";
   templateUrl: './country-list.component.html',
 })
 export class CountryListComponent {
+  countries = input.required<Country[]>();
 
-countries = input.required<Country[]>()
+  errorMessage = input<string | unknown | null>();
+  isLoading = input<boolean>(false);
+  isEmpy = input<boolean>(false);
 
 }
--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { map, Observable, catchError, throwError } from 'rxjs';
+import { map, Observable, catchError, throwError, delay } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mapper/country.mapper';
 
@@ -38,6 +38,7 @@ export class CountryService {
 
     return this.http.get<RESTCountry[]>(url).pipe(
         map( (resp) => CountryMapper.mapRestCountryArrayToCountryArray(resp)),
+        delay(2000),
         catchError(error =>{
           console.log('Error fetching',error);
 
